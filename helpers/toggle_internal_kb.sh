62fe210538163fb589dbed5e3cb64f06fa24f6f2
Paul Calot <paul.calot@datanumia.com>
2026-06-08 09:10:20 +0200

    fix: was not working well, now i have bash script to toggle the keyboard

--- helpers/toggle_internal_kb.sh
+++ helpers/toggle_internal_kb.sh
@@ -1,10 +1,16 @@
 #!/bin/bash
 
 INTERNAL_ID=$(xinput list --id-only "AT Translated Set 2 keyboard")
-EXTERNAL_NAME="Keychron K2"
+MASTER_ID=$(xinput list --id-only "Virtual core keyboard")   
+echo $INTERNAL_ID 
+echo $MASTER_ID
 
-if xinput list | grep -q "$EXTERNAL_NAME"; then
-    xinput disable "$INTERNAL_ID"
+if xinput list "$INTERNAL_ID" | grep -q "floating slave"; then
+    echo "Keyboard is currently disabled. Enabling..."
+    xinput reattach "$INTERNAL_ID" "$MASTER_ID"
+    echo "Keyboard (ID: $INTERNAL_ID) has been successfully enabled."
 else
-    xinput enable "$INTERNAL_ID"
+    echo "Keyboard is currently enabled. Disabling..."
+    xinput float "$INTERNAL_ID"
+    echo "Keyboard (ID: $INTERNAL_ID) has been successfully disabled."
 fi
